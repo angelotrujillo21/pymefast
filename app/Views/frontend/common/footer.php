f6e350a67ec67c1661354d8a2937884c2a6b7065
DESKTOP-OUJHR33\angelo <angelotrujillo210200@gmail.com>
2020-10-17 17:12:04 -0500

    Correcion footernobile

--- app/Views/frontend/common/footer.php
+++ app/Views/frontend/common/footer.php
@@ -3,13 +3,13 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <!-- Footer Location-->
-                    <div class="col-lg-3 mb-5 mb-lg-0 d-flex align-items-center justify-content-start">
+                    <div class="col-lg-3 mb-5 mb-lg-0 d-flex align-items-center  justify-content-center justify-content-md-start">
                         <img class="logo-footer" src="<?= src('multi/logo_bbva.svg') ?>" alt="logo">
                     </div>
 
                     <!-- Footer Social Icons-->
                     <div class="offset-lg-6 col-lg-3 mb-5 mb-lg-0">
-                        <div class="w-100 d-flex align-items-center justify-content-end">
+                        <div class="w-100 d-flex align-items-center justify-content-center justify-content-md-end">
                             <a class="btn btn-icon-footer mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
                             <a class="btn btn-icon-footer mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
                             <a class="btn btn-icon-footer mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
@@ -20,13 +20,13 @@
 
                 <div class="row footer-end">
                     <!-- Footer Location-->
-                    <div class="col-lg-3 mb-5 mb-lg-0  d-flex align-items-center justify-content-starts">
+                    <div class="col-lg-3 mb-5 mb-lg-0  d-flex align-items-center justify-content-center justify-content-md-start">
                         <span>© 2020 Banco Bilbao Vizcaya Argentaria, S.A.</span>
                     </div>
 
                     <!-- Footer Social Icons-->
                     <div class="offset-lg-6 col-lg-3 mb-5 mb-lg-0">
-                        <div class="w-100 d-flex align-items-center justify-content-end">
+                        <div class="w-100 d-flex align-items-center  justify-content-center justify-content-md-end">
                             <span class="text-color-footer">Creando oportunidades</span>
                         </div>
                     </div>
