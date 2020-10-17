6865dcb72d79fd73be0b777a95966f02c8b3f64b
DESKTOP-6NLK6E7\santiagoqc <santiagoqc>
2020-10-17 18:02:02 -0500

    santiago-cambio-2

--- app/Views/frontend/common/navbar.php
+++ app/Views/frontend/common/navbar.php
@@ -1,21 +1,53 @@
-   <!-- Navigation-->
-   <nav class="navbar navbar-expand-lg bg-banca text-uppercase fixed-top" id="mainNav">
-       <div class="container">
-           <a class="navbar-brand js-scroll-trigger" href="#page-top">
-               <img src="<?= src('multi/logo_bbva.svg') ?>" width="180px">
+<!-- Navigation-->
+<style type="text/css">
+    .nav-item{
+        border-bottom: 8px solid transparent;
+    }
+    .nav-item:hover{
+        border-bottom: 8px solid white;
+    }
+    
+    .menu {
+        width: 100%;
+        float: left;
+      }
 
-           </a>
-           <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-               Menu
-               <i class="fas fa-bars"></i>
-           </button>
-           <div class="collapse navbar-collapse" id="navbarResponsive">
-               <ul class="navbar-nav ml-auto">
-                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Personas</a></li>
-                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Empresas</a></li>
-                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Hazte Cliente</a></li>
-               </ul>
-           </div>
-       </div>
-   </nav>
-   <!-- Masthead-->
+      .menu ul li {
+        float: right;
+        list-style-type: none;
+        text-align: left;
+      }
+
+      .menu ul li a {
+        padding: 10px;
+      }
+
+    
+</style>
+
+
+<nav class="navbar navbar-expand-lg bg-banca text-uppercase fixed-top" id="mainNav">
+    <div class="container-fluid">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" >
+            <img src="<?= src('multi/logo_bbva.svg') ?>" width="180px">
+        </a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive" style="margin-left: 5rem;">
+            <ul class="navbar-nav">
+<!--            <ul class="navbar-nav ml-auto">-->
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio" style="font-size: 2em;">Personas</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about" style="font-size: 2em;">Empresas</a></li> 
+            </ul>
+            <div class="menu">
+                <ul>
+                    <li><a href="" style="font-size: 2em; background-color: #02a5a5; color:white; text-transform: capitalize  !important;">BBVA Credi Net</a></li>
+                    <li><a href="" style="font-size: 2em; color:#efb728; margin-right: 1rem; text-transform: capitalize  !important;"><i class="fa fa-user-plus"></i> Hazte cliente</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<!-- Masthead-->
